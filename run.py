10d81dd46dbf303da16dbcc35f081e6f45fe7454
Vizz-VS <v.pregadio.vp@gmail.com>
2026-04-29 07:26:42 +0200

    Removed used stuff and added comment for Navoos ( it's about the setup)

--- run.py
+++ run.py
@@ -331,3 +331,11 @@ if __name__ == "__main__":
                 os.remove(g.composefile)
     except KeyboardInterrupt:
         print()
+
+
+"""
+TL:DR
+
+The code is freaking incredible , litterally this build automitically the whole farm up , dinamically.
+@navoos read it.
+"""
